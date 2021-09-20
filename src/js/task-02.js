490543addc7ba4d94eea42b97fab1189506f9ecc
OleksandrPivets <sashapivets@gmail.com>
2021-09-20 12:15:08 +0300

    homework

---
+++ src/js/task-02.js
@@ -0,0 +1,16 @@
+const ingredients = [
+    'Картошка',
+    'Грибы',
+    'Чеснок',
+    'Помидоры',
+    'Зелень',
+    'Приправы',
+  ];
+  const ingredientsListEl = document.querySelector('#ingredients');
+
+const newIngredientsListEl = ingredients.map(ingredient => {
+ const liEl = document.createElement('li');
+ liEl.innerText = ingredient;
+ return liEl;
+});
+ingredientsListEl.append(...newIngredientsListEl);
---
+++ src/js/task-06.js
@@ -0,0 +1,18 @@
+  
+const inputEl = document.querySelector('#validation-input');
+const validDataLength = inputEl.getAttribute('data-length');
+
+inputEl.addEventListener('blur', onBlurValidation);
+
+function toggleClass(revomeClass, addClass) {
+ inputEl.classList.remove(revomeClass);
+ inputEl.classList.add(addClass);
+}
+
+function onBlurValidation() {
+ if (inputEl.value.length === Number(validDataLength)) {
+  toggleClass('invalid', 'valid');
+ } else {
+  toggleClass('valid', 'invalid');
+ }
+}
